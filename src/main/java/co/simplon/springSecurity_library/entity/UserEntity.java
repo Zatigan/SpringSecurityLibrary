9648abab8f1e5d76479ba33d494124c2d38171dd
BOULET Pierre <pierre.boulet@maif.fr>
2026-08-27 16:33:26 +0200

    feat: add constructors and getters/setters for userEntity

--- src/main/java/co/simplon/springSecurity_library/entity/UserEntity.java
+++ src/main/java/co/simplon/springSecurity_library/entity/UserEntity.java
@@ -25,9 +25,48 @@ public class UserEntity {
             unique = true)
     private String email;
 
+    public UserEntity() {
+    }
+
+    public UserEntity(@Nonnull String name, @Nonnull String email, @Nonnull String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     @Nonnull
     @Column(length = 50,
             nullable = false)
     private String password;
 
+    public UUID getId() {
+        return id;
+    }
+
+    @Nonnull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@Nonnull String name) {
+        this.name = name;
+    }
+
+    @Nonnull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Nonnull String email) {
+        this.email = email;
+    }
+
+    @Nonnull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Nonnull String password) {
+        this.password = password;
+    }
 }
