342cff56840ec814ce5064c9f495221b498744bf
Pierre <66121468+Zatigan@users.noreply.github.com>
2026-08-27 16:54:38 +0200

    Feature/create entities (#1)
    * feat: userEntity ok
    
    * feat: add constructors and getters/setters for userEntity
    
    * feat: BookEntity created
    
    * chore: fix tables names, added comments
    
    ---------
    
    Co-authored-by: BOULET Pierre <pierre.boulet@maif.fr>

---
+++ src/main/java/co/simplon/springSecurity_library/entity/BookEntity.java
@@ -0,0 +1,50 @@
+package co.simplon.springSecurity_library.entity;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Getter // Génère les getters automatiquement
+@Setter // Génère les setters automatiquement
+@NoArgsConstructor // Génère un constructeur vide
+@AllArgsConstructor // Génère un constructeur plein
+@Table(name="book")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Nonnull
+    @Column(length = 250,
+            nullable = false)
+    private String title;
+
+    @Nonnull
+    @Column(length = 150,
+            nullable = false)
+    private String author;
+
+    @Nonnull
+    @Column(length = 100,
+            nullable = false)
+    private String category;
+
+    @Nonnull
+    @Column(length = 8,
+            nullable = false)
+    private Date publishedYear;
+
+    @Nonnull
+    @Column
+    @ColumnDefault("1")
+    private Byte availableCopies;
+}
---
+++ src/main/java/co/simplon/springSecurity_library/entity/UserEntity.java
@@ -0,0 +1,73 @@
+package co.simplon.springSecurity_library.entity;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Nonnull //Annotation pour dire à Java que ça doit pas être null
+    @Column(length = 100,
+            nullable = false, // Indication côté bdd que le champ ne peut pas être null
+            unique = true)  // Indication que chaque valeur sera unique
+    private String name;
+
+    @Nonnull
+    @Column(length = 100,
+            nullable = false,
+            unique = true)
+    private String email;
+
+    // constructeur vide (ça sert toujours)
+    public UserEntity() {
+    }
+    // constructeur plein
+    public UserEntity(@Nonnull String name, @Nonnull String email, @Nonnull String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Nonnull
+    @Column(length = 50,
+            nullable = false)
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Nonnull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@Nonnull String name) {
+        this.name = name;
+    }
+
+    @Nonnull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Nonnull String email) {
+        this.email = email;
+    }
+
+    @Nonnull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Nonnull String password) {
+        this.password = password;
+    }
+}
