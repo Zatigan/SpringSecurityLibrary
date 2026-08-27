c9d4250ec2abbe8913fd131be59d171f451378ab
BOULET Pierre <pierre.boulet@maif.fr>
2026-08-27 10:53:26 +0200

    Initial commit, for real...

---
+++ src/main/java/co/simplon/springSecurity_library/SpringSecurityLibraryApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.springSecurity_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/simplon/springSecurity_library/SpringSecurityLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.springSecurity_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
