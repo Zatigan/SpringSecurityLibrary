27afa69fcd9cef28799f17662a8e518a0e286de7
BOULET Pierre <pierre.boulet@maif.fr>
2026-08-28 15:27:19 +0200

    feat: get routes created

--- src/main/java/co/simplon/springSecurity_library/repository/BookRepository.java
+++ src/main/java/co/simplon/springSecurity_library/repository/BookRepository.java
@@ -4,7 +4,11 @@ import co.simplon.springSecurity_library.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, String> {
 
+    public Optional<BookEntity> findById(UUID id);
 }
